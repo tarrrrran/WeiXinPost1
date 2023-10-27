f6a1da5440d38d4e31c8138a8a88f236499e06be
tarrrrran <103093577+tarrrrran@users.noreply.github.com>
2023-10-27 19:24:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx******************"
+app_id = "wx129a710eb4ecec56"
 # 公众号appSecret
-app_secret = "f9a80260d33******************"
+app_secret = "dfa2d922294673ee7c35efb7dc2891fa"
 # 模板消息id
 # 每日消息
-template_id1 = "wX-************************************"
+template_id1 = "R6DyZc_XJFveEYJiNmbHhEqB7QE8Yu9lurRCqST0clg"
 # 课程消息,上课提醒
-template_id2 = "loe1yHWr************************************"
+# template_id2 = "loe1yHWr************************************"
 # 晚安心语
-template_id3 = "-fun9-2************************************"
+# template_id3 = "-fun9-2************************************"
 # 接收公众号消息的微信号
 # 这是openid
-user = ["ojhdR50T******************"]
+user = ["oMtHK6GgbMp3t6qeCs1Q9VGDlXUw"]
 
 # 信息配置
 # 所在省份
